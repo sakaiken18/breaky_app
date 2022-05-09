71bfe9e8f405bb519d457fe240d1cde074825a23
sakaiken18 <sakaiken6@gmail.com>
2022-05-10 08:26:48 +0900

    ルーティングを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/new'
-  get 'posts/create'
-  get 'posts/edit'
-  get 'posts/update'
-  get 'posts/destroy'
-  root 'breakys#index' 
-  
+  root 'posts#index' 
+  resources :posts
 end
